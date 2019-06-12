73d3f55c5c1809ba540bcfd528ce5ff75732c3fd
Jorengarenar <>
2019-06-12 15:50:03 +0200

    Item Counter starts to refresh when changing page

--- Pocket_Items_Counter.user.js
+++ Pocket_Items_Counter.user.js
@@ -1,21 +1,24 @@
-
 // ==UserScript==
 // @name         Pocket - Items Counter
 // @description  DROPPED | As more items load when you scroll down, you can refresh the countItems hitting Control-Backspace.
 // @author       Jorengarenar
-// @version      1.2.2
+// @version      1.2.3
 // @include      *://app.getpocket.com*
 // @run-at       document-end
 // ==/UserScript==
 
 function countItems(){
-    // document.querySelector('.item_type_spoc').remove();
+    try {
+        document.querySelector('#counter').remove();
+    } catch(e) {
+        console.log(e)
+    }
     let number = document.getElementsByClassName('css-d4njwk').length;
     console.log(number);
     let intext = document.createElement('li');
     intext.id = "counter";
     intext.className = "css-1q7nt9b";
-    intext.innerHTML = 'Count: <span style="font-weight:bold; color: red;">' + number + '</span>';
+    intext.innerHTML = '<a><span style="padding: 5px">Count:</span><span style="font-weight:bold; color: red;">' + number + '</span></a>';
     document.querySelector('.css-7gaew3').appendChild(intext);
 }
 
@@ -26,18 +29,22 @@ function countItems(){
 //     }
 // });
 
-// $('div, a').parent().click(function() {
-//     window.setTimeout(function(){
-//         countItems();
-//     }, 500);
-// });
 //
 // $('.wrapper_tag').parent().click(function() {
 //     countItems();
 // });
 
-function countItems2() {
+function initialize() {
     window.setTimeout(function(){
+    let test = document.querySelectorAll('.css-1s7mmnq a')
+    console.log(test);
+    for (let x of test) {
+        x.addEventListener('click', function(){
+            window.setTimeout(function(){
+                countItems();
+            }, 500);
+        }, false);
+    }
         if (document.getElementsByClassName('item_content').length - document.getElementsByClassName('tf-hidden').length < 30) {
             window.setTimeout(function(){
                 countItems();
@@ -58,4 +65,4 @@ function countItems2() {
 //     countItems2();
 // });
 
-window.onload = countItems2();
+window.onload = initialize();
