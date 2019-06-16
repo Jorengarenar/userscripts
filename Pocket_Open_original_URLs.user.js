daaec0be6c319b23838ce2d260c78aadddc95d87
Jorengarenar <>
2019-06-16 11:50:26 +0200

    Pocket - Open original URLs -- now it will work without refreshing page

--- Pocket_Open_original_URLs.user.js
+++ Pocket_Open_original_URLs.user.js
@@ -2,21 +2,35 @@
 // @name         Pocket - Open original URLs
 // @description  Opens original links instead of article view
 // @author       Jorengarenar
-// @version      0.1
+// @version      0.2
 // @include      http*://app.getpocket.com*
 // @include      http*://getpocket.com/*
 // @run-at       document-end
 // ==/UserScript==
 
-window.setTimeout(function() {
-    let articles = document.querySelectorAll('article');
-    for (let link of articles) {
-        let originalURL = link.querySelector('.css-1fzr42x > a');
-        if (originalURL) {
-            let articleURL = link.querySelector('.css-7zhfhb > a');
-            let originalCopy = originalURL.cloneNode(1);
-            originalCopy.innerHTML = articleURL.innerHTML;
-            link.querySelector('.css-7zhfhb').replaceChild(originalCopy, articleURL);
+
+const config = {
+    childList: true,
+    subtree: true
+};
+
+function change(queue, observer) {
+    window.setTimeout(function() {
+        let articles_list = document.querySelectorAll('article');
+        for (let link of articles_list) {
+            let originalURL = link.querySelector('.css-1fzr42x > a');
+            if (originalURL) {
+                let articleURL = link.querySelector('.css-7zhfhb > a');
+                let originalCopy = originalURL.cloneNode(1);
+                originalCopy.innerHTML = articleURL.innerHTML;
+                link.querySelector('.css-7zhfhb').replaceChild(originalCopy, articleURL);
+            }
         }
-    }
-}, 1000);
+    }, 1000);
+};
+
+const observer = new MutationObserver(change);
+
+change(document.body, config);
+
+observer.observe(document.body, config);
