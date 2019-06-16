b62a4bf5b97e9602878876e0db065867a9223896
Jorengarenar <>
2019-06-16 13:19:10 +0200

    Pixiv RSS -- now works for unlogged users too

--- Pixiv_RSS_Button.user.js
+++ Pixiv_RSS_Button.user.js
@@ -1,21 +1,25 @@
 // ==UserScript==
-// @name         Pixiv - RSS Buttons
+// @name         Pixiv - RSS Button
 // @description  RSS using RSSHub.app
 // @author       Jorengarenar
-// @version      0.2
+// @version      0.3
 // @run-at       document-end
 // @include      https://www.pixiv.net
 // @include      *pixiv.net*
-// @namespace https://greasyfork.org/users/309959
 // ==/UserScript==
 
 let rssButton = document.createElement('a');
 rssButton.target = "_blank";
 rssButton.innerHTML = "<b>RSS</b>";
 rssButton.setAttribute('class', "_2Of8xxg");
-rssButton.style = "margin-right: 10px";
-rssButton.href = "https://rsshub.app/pixiv/user/" + window.location.href.match(/id=(\d+)/)[1];
+rssButton.style = "margin-right: 10px; margin-left: 20px";
+let id = window.location.href.match(/id=(\d+)/)[1];
+rssButton.href = "https://rsshub.app/pixiv/user/" + id;
 window.onload = function() {
-    let bar = document.querySelector('._310Z6Ex');
-    bar.insertBefore(rssButton, bar.childNodes[0]);
+    let bar = document.querySelector('._310Z6Ex')
+    if (bar) {
+        bar.insertBefore(rssButton, bar.childNodes[0]);
+    } else {
+        document.querySelector('.userdata').insertBefore(rssButton, document.querySelector('.count-container'));
+    }
 };
