ea1e7440761bac2b5905be4e4f7833bae1821cba
Jorengarenar <>
2019-06-16 13:01:35 +0200

    Styling

--- Pixiv_RSS_Button.user.js
+++ Pixiv_RSS_Button.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
-// @name         Pixiv - RSS Button
+// @name         Pixiv - RSS Buttons
 // @description  RSS using RSSHub.app
 // @author       Jorengarenar
-// @version      0.1
+// @version      0.2
 // @run-at       document-end
 // @include      https://www.pixiv.net
 // @include      *pixiv.net*
+// @namespace https://greasyfork.org/users/309959
 // ==/UserScript==
 
 let rssButton = document.createElement('a');
 rssButton.target = "_blank";
 rssButton.innerHTML = "<b>RSS</b>";
 rssButton.setAttribute('class', "_2Of8xxg");
-let id = window.location.href.match(/id=(\d+)/)[1];
-rssButton.href = "https://rsshub.app/pixiv/user/" + id;
+rssButton.style = "margin-right: 10px";
+rssButton.href = "https://rsshub.app/pixiv/user/" + window.location.href.match(/id=(\d+)/)[1];
 window.onload = function() {
-    document.querySelector('._310Z6Ex').appendChild(rssButton);
+    let bar = document.querySelector('._310Z6Ex');
+    bar.insertBefore(rssButton, bar.childNodes[0]);
 };
