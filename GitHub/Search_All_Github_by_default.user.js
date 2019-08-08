ef98db77aff815ebbbe3225744af87cd8ed6b1ba
Jorengarenar <jorengarenar@protonmail.com>
2019-08-08 21:46:38 +0200

    Cleaned code & used proper RegEx in include rule

--- GitHub/Search_All_Github_by_default.user.js
+++ GitHub/Search_All_Github_by_default.user.js
@@ -1,31 +1,26 @@
 // ==UserScript==
 // @name         Search "All Github" by default
-// @description  When you are on repository page selects "All GitHub" by default instead of "In this repository"
+// @description  When you are on repository/user page selects "All GitHub" by default instead of "In this repository/user"
 // @author       Jorengarenar
 // @homepageURL  https://joren.ga
-// @version      0.1
+// @version      0.2
 // @run-at       document-end
-// @include      https://github.com*
+// @include      /https?:\/\/github\.com\/.+/
 // ==/UserScript==
 
 const config = {
-    childList: true,
-    subtree: true
+  childList: true,
+  subtree: true
 };
 
-function swapDiv(event,elem){
-    elem.parentNode.insertBefore(elem,elem.parentNode.firstChild);
-}
+const observer = new MutationObserver( () => {
+  let inThisSearch = document.getElementById('jump-to-suggestion-search-scoped');
+  inThisSearch.setAttribute("aria-selected", "false");
+  inThisSearch.classList.remove("navigation-focus");
 
-function change(queue, observer) {
-        let repoSearch =  document.querySelector('#jump-to-suggestion-search-scoped');
-        repoSearch.setAttribute("aria-selected", "false");
-        repoSearch.classList.remove("navigation-focus");
-        let globalSearch =  document.querySelector('#jump-to-suggestion-search-global')
-        globalSearch.setAttribute("aria-selected", "true")
-        globalSearch.classList.add("navigation-focus");
-}
-
-const observer = new MutationObserver(change);
+  let globalSearch = document.getElementById('jump-to-suggestion-search-global');
+  globalSearch.setAttribute("aria-selected", "true");
+  globalSearch.classList.add("navigation-focus");
+});
 
 observer.observe(document.querySelector('.header-search'), config);
