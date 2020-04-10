8c391482ad1feff4088f507190507ed35a659c4e
Jorengarenar <jorengarenar@protonmail.com>
2020-04-10 07:39:32 +0200

    Fix URL

--- Pixiv/RSS_Button.user.js
+++ Pixiv/RSS_Button.user.js
@@ -3,23 +3,18 @@
 // @description  RSS using RSSHub.app
 // @author       Jorengarenar
 // @homepageURL  https://joren.ga
-// @version      0.3.2
+// @version      0.3.3
 // @run-at       document-end
-// @include      /https?:\/\/www\.pixiv\.net\/member.*\.php\?id=\d+/
+// @include      /https?:\/\/www\.pixiv\.net\/.*users\/\d+/
 // ==/UserScript==
 
 let rssButton = document.createElement('a');
 rssButton.target = "_blank";
 rssButton.innerHTML = "<b>RSS</b>";
 rssButton.setAttribute('class', "_2Of8xxg");
-rssButton.style = "margin-right: 10px; margin-left: 20px";
-let id = window.location.href.match(/id=(\d+)/)[1];
+let id = window.location.href.match(/users\/(\d+)/)[1];
 rssButton.href = "https://rsshub.app/pixiv/user/" + id;
-window.onload = function() {
-  let bar = document.querySelector('._310Z6Ex')
-  if (bar) {
-    bar.insertBefore(rssButton, bar.childNodes[0]);
-  } else {
-    document.querySelector('.userdata').insertBefore(rssButton, document.querySelector('.count-container'));
-  }
-};
+window.setTimeout(function() {
+  let bar = document.querySelector('._310Z6Ex');
+  if (bar) { bar.insertBefore(rssButton, bar.childNodes[1]); }
+}, 1000);
